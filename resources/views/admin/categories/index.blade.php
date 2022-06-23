e898299b5e9165e5afb86c70052cdef8c65afa9c
SirAlyon <alessiosada04@gmail.com>
2022-06-24 01:08:45 +0200

    another script fix :D

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -37,10 +37,11 @@
                             
                             <td>{{$category->id}}</td>
                             <td>
+                                <div id="label-update-{{$category->id}}" class="py-2" style="min-width: 350px;">{{$category->name}}</div>
                                 <form action="{{route('admin.categories.update', $category->id)}}" method="post" id="cat-update-{{$category->id}}" class="category-update">
                                     @csrf
                                     @method('PATCH')
-                                    <input type="text" name="name" id="input-update-{{$category->id}}" value="{{$category->name}}" class="form-control b-none bg-transparent cat-update-{{$category->id}}" placeholder="Add a new category" aria-label="Category name" aria-describedby="button-addon2">
+                                    <input type="text" name="name" id="input-update-{{$category->id}}" value="{{$category->name}}" class="form-control b-none d-none bg-transparent cat-update-{{$category->id}}" placeholder="Add a new category" aria-label="Category name" aria-describedby="button-addon2">
                                 </form>
                                
                             </td>
@@ -50,7 +51,7 @@
                             </td>
                             <td class="d-flex">
 
-                                <button type="submit"  form="cat_update_{{$category->id}}" onclick="checkInputUpdate(event)" class="btn btn-secondary" id="btn-update-{{$category->id}}">
+                                <button type="submit"  form="cat_update_{{$category->id}}" onclick="checkInputUpdate(event, {{$category->id}})" class="btn btn-secondary" id="btn-update-{{$category->id}}">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square pe-none" viewBox="0 0 16 16">
                                         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                                         <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
@@ -83,18 +84,28 @@
 
 @section('page_script')
     <script type="text/javascript">
-        function checkInputUpdate(event) {
+        function checkInputUpdate(event, id) {
             console.log(event.target);
 
-            let cat_id = event.target.id.replace("btn", "input");
-            let input = document.getElementById(cat_id)
+            //let cat_id = event.target.id.replace("btn", "input");
+            let input = document.getElementById('input-update-' + id)
+            let label = document.getElementById('label-update-' + id)
+
+
+            input.classList.remove('d-none')
+            label.classList.add('d-none')
+
+            console.log(id, input);
+
+            
+            
 
             //console.log(cat_id);
 
             //console.log(input.defaultValue, input.value);
 
             let forms = document.querySelectorAll('.category-update')
-            console.log(forms);
+            //console.log(forms);
             if (input.defaultValue === input.value) {
 
                 let inputValue = input.value
@@ -107,7 +118,7 @@
 
             forms.forEach(form => {
                     form.id = form.id.replace('-', '_')
-                    console.log(form);
+                    //console.log(form);
             });
         }
     </script>
